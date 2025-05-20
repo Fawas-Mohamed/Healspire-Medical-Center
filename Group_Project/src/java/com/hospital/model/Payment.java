3e44188b94b4bd8ca524b979e607e2c2c45d73c9
ifas777 <ifasiqbal490@gmail.com>
2025-05-20 14:42:28 +0530

    Create Payment.java

---
+++ Group_Project/src/java/com/hospital/model/Payment.java
@@ -0,0 +1,48 @@
+package com.hospital.model;
+import java.util.Date;
+public class Payment {
+     private int id;
+    private int patientId;
+    private double amount;
+    private Date paymentDate;
+    private String status;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    } 
+}
