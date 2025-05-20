a2f4577862347efed7487b8ebfb14fd716e57111
ifas777 <ifasiqbal490@gmail.com>
2025-05-20 14:25:22 +0530

    Create Doctor.java

---
+++ Group_Project/src/java/com/hospital/model/Doctor.java
@@ -0,0 +1,81 @@
+package com.hospital.model;
+public class Doctor {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the specialization
+     */
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    /**
+     * @param specialization the specialization to set
+     */
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    /**
+     * @return the fee
+     */
+    public double getFee() {
+        return fee;
+    }
+
+    /**
+     * @param fee the fee to set
+     */
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+     private int id;
+    private String name;
+    
+    private String specialization;
+    private double fee;
+    private String password;
+
+   
+}
